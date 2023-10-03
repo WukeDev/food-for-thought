acefa31dba08d1f9e1efdd659f95cbc6d0569ebe
amplitude-mc <86304082+amplitude-mc@users.noreply.github.com>
2023-10-03 16:24:49 +0000

    Working on php for spreadsheet editing

---
+++ navbar.php
@@ -0,0 +1,11 @@
+<nav class="navbar" id="navbar">
+    <div class="container">
+        <div class="logo">Food For Thought</div>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="schedule.html">Schedule a Session</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="developers.html">Developers</a></li>
+        </ul>
+    </div>
+</nav>
