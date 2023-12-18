f2cf1424726f686c1ea099a90839cbd42e097b3d
amplitude-mc <86304082+amplitude-mc@users.noreply.github.com>
2023-12-18 04:08:17 +0000

    Fixed bug

--- calendar.js
+++ calendar.js
@@ -1,4 +1,4 @@
-onsole.log("calendar");
+console.log("calendar");
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js";
